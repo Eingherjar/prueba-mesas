288b2d30be965aef438c35d1511a88e20332e137
gianluigi081 <gianluigiher@gmail.com>
2020-06-11 18:09:34 -0500

    Vistas del administrador

--- proyecto/src/app/shared/buscador/buscador.component.ts
+++ proyecto/src/app/shared/buscador/buscador.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-buscador',
   templateUrl: './buscador.component.html',
-  styleUrls: ['./buscador.component.scss']
+  styleUrls: ['./buscador.component.scss'],
 })
 export class BuscadorComponent implements OnInit {
+  constructor() {}
+  condicion: string = 'vista_mesas_qr';
 
-  constructor() { }
-  condicion:string = "menu_principal";
-  ngOnInit(): void {
+  active: boolean = false;
+
+  con = 0;
+  ngOnInit(): void {}
+
+  contador(condicion) {
+    if (condicion == true) {
+      this.con++;
+    } else {
+      this.con--;
+    }
   }
-
 }
